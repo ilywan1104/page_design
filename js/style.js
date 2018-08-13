2df310a8722549f565af69cc9567162cc513c102
ilywan1104 <ilywan1104@live.com>
2018-08-13 16:25:56 +0800

    commit

--- js/style.js
+++ js/style.js
@@ -1,3 +1,12 @@
-$(document).ready(function(){
-  $('body').append('<h1>Hello World!</h1>');
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    anchors: ['firstPage', 'secondPage', '3rdPage', '4rdPage', '5rdPage'],
+    sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C', '#1BBC9B', '#7E8F7C'],
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: ['First page', 'Second page', 'Third and last page'],
+    responsiveWidth: 1100
+  });
+
+  //$.fn.fullpage.setAllowScrolling(true);
 });
