4fd8ecfdaf84f7881c134a3ec7260f1035bc010f
ilywan1104 <ilywan1104@live.com>
2018-08-18 19:09:50 +0800

    commit

--- js/style.js
+++ js/style.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
     anchors: ['firstPage', 'secondPage', '3rdPage', '4rdPage', '5rdPage'],
-    sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C', '#1BBC9B', '#7E8F7C'],
+    //sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C', '#1BBC9B', '#7E8F7C'],
     navigation: true,
     navigationPosition: 'right',
     navigationTooltips: ['First page', 'Second page', 'Third and last page'],
