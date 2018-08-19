d547b6d37b1e59dad09d1501621d23a446e5e565
ilywan1104 <ilywan1104@live.com>
2018-08-19 15:10:46 +0800

    commit

--- js/style.js
+++ js/style.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    anchors: ['firstPage', 'secondPage', '3rdPage', '4rdPage', '5rdPage'],
+    // anchors: ['firstPage', 'secondPage', '3rdPage', '4rdPage', '5rdPage'],
     //sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C', '#1BBC9B', '#7E8F7C'],
     navigation: true,
     navigationPosition: 'right',
     navigationTooltips: ['First page', 'Second page', 'Third and last page'],
     responsiveWidth: 768,
+    verticalCentered: true,
     afterLoad: function(anchorLink, index){
 			if(index == 1){
-        $('.bn-bg-00,.bn-bg-01,.bn-bg-02').addClass('d-block fadeInDown animated');
-        setTimeout(function() {
-          $('.bn-bg-03').addClass('fadeIn animated d-block');
-        },800);
-
+        $('.bn-bg-00,.bn-bg-01,.bn-bg-02,.bn-bg-03').addClass('fadeInDown animated');
 			}
 			//$('#infoMenu').toggleClass('whiteLinks', index ==4);
-		}
+		},
+    onLeave: function(index, nextIndex, direction){
+
+  	}
   });
 
   //$.fn.fullpage.setAllowScrolling(true);
