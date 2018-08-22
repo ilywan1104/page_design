bf07d7d23209a8e56705c5c0c8232b56c0127594
ilywan1104 <ilywan1104@live.com>
2018-08-22 14:08:14 +0800

    commit

--- js/style.js
+++ js/style.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    // anchors: ['firstPage', 'secondPage', '3rdPage', '4rdPage', '5rdPage'],
-    //sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C', '#1BBC9B', '#7E8F7C'],
     navigation: true,
     navigationPosition: 'right',
     navigationTooltips: ['First page', 'Second page', 'Third and last page'],
@@ -11,11 +9,6 @@ $(document).ready(function() {
 			if(index == 1){
         $('.bn-bg-00,.bn-bg-01,.bn-bg-02,.bn-bg-03').addClass('fadeInDown animated');
 			}
-		},
-    onLeave: function(index, nextIndex, direction){
-
-  	}
+		}
   });
-
-  //$.fn.fullpage.setAllowScrolling(true);
 });
